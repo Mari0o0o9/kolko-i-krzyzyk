e1ba89651e1bff1eb66cd0ce13a2db291218164f
Mari0 <discordtako8@gmail.com>
2023-12-15 19:46:13 +0100

    modified:   index.html 	modified:   main.js 	modified:   style.css

---
+++ main.js
@@ -0,0 +1,15 @@
+var tabBlok = [
+    [1,2,3],
+    [0,0,0],
+    [0,0,0]
+]
+
+function znak() {
+    var znak = document.querySelectorAll('.blok')
+    znak.forEach(element => {
+        element.addEventListener('click', function () {
+            
+        })
+    })
+}
+znak();
